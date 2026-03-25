c32d6fd26397930183c9bc47059fb49d72d2660a
Antonio Claudio <claudioalvesdev@gmail.com>
2026-03-25 19:05:00 -0300

    feat: initial project with mix

---
+++ hello/config/test.exs
@@ -0,0 +1,38 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :hello, Hello.Repo,
+  database: ":memory:",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello, HelloWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "jv0UxLviK+ZlqYz4D6RnlRT2Pf6L22Co+QexxsCXtOQNvf4kX2lQwxADK4GkKX5v",
+  server: false
+
+# In test we don't send emails
+config :hello, Hello.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
+
+# Sort query params output of verified routes for robust url comparisons
+config :phoenix,
+  sort_verified_routes_query_params: true
